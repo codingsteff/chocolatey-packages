784666e6ef4e5ade5f17001d33490ea1b4151ce9
codingsteff <coding.steff@gmail.com>
2014-01-14 12:11:24 +0100

    Chocolatey package for TeamViewer Host added
    Signed-off-by: codingsteff <coding.steff@gmail.com>

---
+++ teamviewer.host/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'teamviewer.host'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.teamviewer.com/download/TeamViewer_Host_Setup.exe'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
