c556b1f3544c6aedec08b87040f849804b2e1a66
codingsteff <coding.steff@gmail.com>
2016-08-30 16:03:22 +0200

    script new-package from _template

---
+++ _template/tools/chocolateyInstallExe.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = ''
+$url32       = ''
+$url64       = ''
+$checksum32  = ''
+$checksum64  = ''
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\$packageName.exe"
+    Write-Host "$packageName registered as $packageName"
+}
+else { Write-Warning "Can't find $PackageName install location" }
