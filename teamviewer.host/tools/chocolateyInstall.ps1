54160145d1b4de986399a02e47f716898e5fbced
codingsteff <coding.steff@gmail.com>
2016-09-02 11:04:14 +0200

    only 32-Bit package

--- teamviewer.host/tools/chocolateyInstall.ps1
+++ teamviewer.host/tools/chocolateyInstall.ps1
@@ -2,9 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe'
-$url64       = $url32
 $checksum32  = '92d79584221e7ad5ffd6820fa9d61e45e4c38eb7693542d471997260119ca0c7'
-$checksum64  = $checksum32
 
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
@@ -12,11 +10,8 @@ $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
   url                    = $url32
-  url64bit               = $url64
   checksum               = $checksum32
-  checksum64             = $checksum64
   checksumType           = 'sha256'
-  checksumType64         = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
