c268d9c395c237e21bc5920d9cb4c045b4dbe467
codingsteff <coding.steff@gmail.com>
2016-12-22 13:25:47 +0100

    au plugins already integrated

--- update_all.ps1
+++ update_all.ps1
@@ -9,6 +9,31 @@ $options = [ordered]@{
     Push    = $true
     Threads = 10
 
+    # Save text report in the local file report.txt
+    Report = @{
+        Type = 'text'
+        Path = "$PSScriptRoot\report.txt"
+    }
+
+    # Then save this report as a gist using your api key and gist id
+    Gist = @{
+        ApiKey = $Env:github_api_key
+        Id     = $Env:github_gist_id
+        Path   = "$PSScriptRoot\report.txt"
+    }
+
+    # Persist pushed packages to your repository
+    Git = @{
+        User = ''
+        Password = $Env:github_api_key
+    }
+
+    # Then save run info which can be loaded with Import-CliXML and inspected
+    RunInfo = @{
+        Path = "$PSScriptRoot\update_info.xml"
+    }
+
+    # Finally, send an email to the user if any error occurs and attach previously created run info
     Mail = if ($Env:mail_user) {
             @{
                 To        = $Env:mail_user
@@ -18,19 +43,7 @@ $options = [ordered]@{
                 Port      = 587
                 EnableSsl = $true
             }
-           } else {}
-
-    Gist_ID = $Env:Gist_ID
-
-    Script = {
-        param($Phase, $Info)
-
-        if ($Phase -ne 'END') { return }
-
-        Save-RunInfo
-        Save-Gist
-        Save-Git
-    }
+           } 
 }
 
 updateall -Name $Name -Options $options | ft
