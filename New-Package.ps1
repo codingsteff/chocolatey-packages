3cabce334b7d15874275e948d91ae63531901fe0
codingsteff <coding.steff@gmail.com>
2017-05-31 16:42:25 +0200

    formatted code

--- New-Package.ps1
+++ New-Package.ps1
@@ -7,51 +7,50 @@ param($Name, $Type)
 .DESCRIPTION
     This function creates a new package by using the directory _template which contains desired package basic settings.
 #>
-function New-Package{
-    [CmdletBinding()]
-    param(
-        #Package name
-        [string] $Name,
+function New-Package {
+  [CmdletBinding()]
+  param(
+    #Package name
+    [string] $Name,
 
-        #Type of the package
-        [ValidateSet('Portable', 'Installer')]
-        [string] $Type='Installer',
+    #Type of the package
+    [ValidateSet('Portable', 'Installer')]
+    [string] $Type = 'Installer',
 
-        #Github repository in the form username/repository
-        [string] $GithubRepository
-    )
+    #Github repository in the form username/repository
+    [string] $GithubRepository
+  )
 
-    if ($Name -eq $null) { throw "Name can't be empty" }
-    if (Test-Path $Name) { throw "Package with that name already exists" }
-    if (!(Test-Path _template)) { throw "Template for the packages not found" }
-    Copy-Item _template $Name -Recurse
+  if ($Name -eq $null) { throw "Name can't be empty" }
+  if (Test-Path $Name) { throw "Package with that name already exists" }
+  if (!(Test-Path _template)) { throw "Template for the packages not found" }
+  Copy-Item _template $Name -Recurse
 
-    $nuspec = Get-Content "$Name\template.nuspec"
-    Remove-Item "$Name\template.nuspec"
+  $nuspec = Get-Content "$Name\template.nuspec"
+  Remove-Item "$Name\template.nuspec"
 
-    Write-Verbose 'Fixing nuspec'
-    $nuspec = $nuspec -replace '<id>.+',               "<id>$Name</id>"
-    $nuspec = $nuspec -replace '<iconUrl>.+',          "<iconUrl>https://cdn.rawgit.com/$GithubRepository/master/$Name/icon.png</iconUrl>"
-    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/$Name</packageSourceUrl>"
-    $nuspec | Out-File -Encoding UTF8 "$Name\$Name.nuspec"
+  Write-Verbose 'Fixing nuspec'
+  $nuspec = $nuspec -replace '<id>.+', "<id>$Name</id>"
+  $nuspec = $nuspec -replace '<iconUrl>.+', "<iconUrl>https://cdn.rawgit.com/$GithubRepository/master/$Name/icon.png</iconUrl>"
+  $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/$Name</packageSourceUrl>"
+  $nuspec | Out-File -Encoding UTF8 "$Name\$Name.nuspec"
 
-    switch ($Type)
-    {
-        'Installer' {
-            Write-Verbose 'Using installer template'
-            Remove-Item "$Name\tools\chocolateyInstallZip.ps1"
-            Move-Item "$Name\tools\chocolateyInstallExe.ps1" "$Name\tools\chocolateyInstall.ps1"
-        }
-        'Portable' {
-            Write-Verbose 'Using portable template'
-            Remove-Item "$Name\tools\chocolateyInstallExe.ps1"
-            Move-Item "$Name\tools\chocolateyInstallZip.ps1" "$Name\tools\chocolateyInstall.ps1"
-        }
+  switch ($Type) {
+    'Installer' {
+      Write-Verbose 'Using installer template'
+      Remove-Item "$Name\tools\chocolateyInstallZip.ps1"
+      Move-Item "$Name\tools\chocolateyInstallExe.ps1" "$Name\tools\chocolateyInstall.ps1"
     }
+    'Portable' {
+      Write-Verbose 'Using portable template'
+      Remove-Item "$Name\tools\chocolateyInstallExe.ps1"
+      Move-Item "$Name\tools\chocolateyInstallZip.ps1" "$Name\tools\chocolateyInstall.ps1"
+    }
+  }
 
-    Write-Verbose 'Fixing chocolateyInstall.ps1'
-    $installer = Get-Content "$Name\tools\chocolateyInstall.ps1"
-    $installer -replace "(^[$]packageName\s*=\s*)('.*')", "`$1'$($Name)'" | Set-Content "$Name\tools\chocolateyInstall.ps1"
+  Write-Verbose 'Fixing chocolateyInstall.ps1'
+  $installer = Get-Content "$Name\tools\chocolateyInstall.ps1"
+  $installer -replace "(^[$]packageName\s*=\s*)('.*')", "`$1'$($Name)'" | Set-Content "$Name\tools\chocolateyInstall.ps1"
 }
 
 New-Package $Name $Type -GithubRepository codingsteff/chocolatey-packages -Verbose
--- _template/update.ps1
+++ _template/update.ps1
@@ -3,25 +3,25 @@ import-module au
 $releases = ''
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+  $download_page = Invoke-WebRequest -Uri $releases
 
-    $re    = ''
-    $url   = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+  $re = ''
+  $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 
-    $version  = $url -split '[._-]|.exe' | Select-Object -Last 1 -Skip 2
+  $version = $url -split '[._-]|.exe' | Select-Object -Last 1 -Skip 2
 
-    return @{ URL32 = $url; Version = $version }
+  return @{ URL32 = $url; Version = $version }
 }
 
 update
