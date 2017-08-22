a48bc78585161bdb5b6019f092d610333c0385e6
Stefan B <codingsteff@users.noreply.github.com>
2017-08-22 14:18:18 +0200

    filed version + checksum

--- teamviewer.host/tools/chocolateyInstall.ps1
+++ teamviewer.host/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe'
-$checksum32  = ''
+$checksum32  = '5d3ed5e53945807c3f1b7e9c96c3d4eea2f70e411583b47ec4b4a0131bce9915'
 
 $packageArgs = @{
   packageName            = $packageName
