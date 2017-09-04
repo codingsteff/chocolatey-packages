77246d888eb1a1880fadf6ca5ddab622008deb0f
appveyor <appveyor>
2017-09-04 22:05:23 +0000

    AU: 1 updated - teamviewer.host [skip ci] https://gist.github.com/11d4abbe6bb1fcd50426bad5b7ea88de/8cedbdf48a840ab22f0f08d23c4a238c6b90dddd

--- teamviewer.host/tools/chocolateyInstall.ps1
+++ teamviewer.host/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe'
-$checksum32  = '013cf5e299e83a7b77e1a7e8c7eb2dcc383e5769119da2d174cd8857318d9720'
+$checksum32  = '0c9213499b22b10e64e580e2bc7147643fa782f39ff42df02e486cbcb40f89bb'
 
 $packageArgs = @{
   packageName            = $packageName
