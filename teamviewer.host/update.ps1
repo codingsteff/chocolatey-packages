028dd77a48b632b79885c1b79acdadb662d923ed
Stefan B <codingsteff@users.noreply.github.com>
2017-12-12 14:26:55 +0100

    Fix hang on Invoke-WebRequest

--- teamviewer.host/update.ps1
+++ teamviewer.host/update.ps1
@@ -13,11 +13,25 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $downloadUri = "https://www.teamviewer.com/en/download/windows/"
-  $page = Invoke-WebRequest -Uri $downloadUri
-  $versionElement = $page.ParsedHtml.body.getElementsByClassName('DownloadVersion') | Select-Object -First 1
-  $version = $versionElement.innerHtml.Replace("v", "")
+  # Bug: Invoke-WebRequest hangs
+  # https://github.com/PowerShell/PowerShell/issues/2812
+  # Workarround UseBasicParsing
+  #$page = Invoke-WebRequest -Uri $downloadUri
+  #$versionElement = $page.ParsedHtml.body.getElementsByClassName('DownloadVersion') | Select-Object -First 1
+  #$version = $versionElement.innerHtml.Replace("v", "")
+  $page = Invoke-WebRequest -Uri $downloadUri -UseBasicParsing
+  $version = extractVersion($page)
 
   return @{ URL32 = $url; Version = $version }
+}
+
+function extractVersion($page) {
+  $locater = "<p class=""DownloadVersion"">"
+  $start = $page.Content.IndexOf($locater)
+  $part = $page.Content.Substring($start+$locater.Length)
+  $end = $part.IndexOf("</p>")
+  $version = $part.Substring(0, $end)
+  return $version
 }
 
 update -ChecksumFor 32
