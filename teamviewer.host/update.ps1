5477d86de23fb1c79467511b1b25ddf71010b966
Stefan B <codingsteff@users.noreply.github.com>
2018-10-12 16:57:26 +0200

    read version from exe

--- teamviewer.host/update.ps1
+++ teamviewer.host/update.ps1
@@ -12,26 +12,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $downloadUri = "https://www.teamviewer.com/en/download/windows/"
-  # Bug: Invoke-WebRequest hangs
-  # https://github.com/PowerShell/PowerShell/issues/2812
-  # Workarround UseBasicParsing
-  #$page = Invoke-WebRequest -Uri $downloadUri
-  #$versionElement = $page.ParsedHtml.body.getElementsByClassName('DownloadVersion') | Select-Object -First 1
-  #$version = $versionElement.innerHtml.Replace("v", "")
-  $page = Invoke-WebRequest -Uri $downloadUri -UseBasicParsing
-  $version = extractVersion($page)
-
+  $tmp = New-TemporaryFile
+  Invoke-WebRequest -Uri $url -OutFile $tmp
+  $version = (Get-Command $tmp).Version
   return @{ URL32 = $url; Version = $version }
-}
-
-function extractVersion($page) {
-  $locater = "v13."
-  $start = $page.Content.IndexOf($locater)
-  $part = $page.Content.Substring($start)
-  $end = $part.IndexOf("</p>")
-  $version = $part.Substring(0, $end)
-  return $version.Replace("v", "")
 }
 
 update -ChecksumFor 32
