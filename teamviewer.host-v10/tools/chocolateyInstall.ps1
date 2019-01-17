497f9325d914ca529901c4d1f90bb1cd5bc9707f
Stefan B <codingsteff@users.noreply.github.com>
2019-01-17 14:05:27 +0100

    refreshed nuspec versions

--- teamviewer.host-v10/tools/chocolateyInstall.ps1
+++ teamviewer.host-v10/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/version_10x/TeamViewer_Host_Setup.exe'
-$checksum32  = ''
+$checksum32  = '7ad7fc01703481b7bdbe4197261b194112c202f1f1f63d49485b284cb46c2143'
 
 $packageArgs = @{
   packageName            = $packageName
--- teamviewer.host-v11/tools/chocolateyInstall.ps1
+++ teamviewer.host-v11/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/version_11x/TeamViewer_Host_Setup.exe'
-$checksum32  = ''
+$checksum32  = 'd8ad7d9f08b9713ad0315e1df3b04197768ba3253aff99bdc7f2237a04a4750a'
 
 $packageArgs = @{
   packageName            = $packageName
