5c33b45a7e611fb09af160f5637af0e3071f0cf1
Stefan B <codingsteff@users.noreply.github.com>
2020-01-30 13:56:44 +0100

    latest v15

--- teamviewer.host-v15/tools/chocolateyInstall.ps1
+++ teamviewer.host-v15/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/version_15x/TeamViewer_Host_Setup.exe'
-$checksum32  = ''
+$checksum32  = 'a0a0ff653b29403aff60d93964a5c7cf8a5e4f7e6692972762e2c24101a0f79d'
 
 $packageArgs = @{
   packageName            = $packageName
