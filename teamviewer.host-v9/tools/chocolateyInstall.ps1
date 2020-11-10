a49deffd673eee7983109c587e9746906df16530
Stefan B <codingsteff@users.noreply.github.com>
2020-11-10 16:52:35 +0100

    v9 added

---
+++ teamviewer.host-v9/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'teamviewer.host'
+$url32       = 'https://download.teamviewer.com/download/version_9x/TeamViewer_Host_Setup.exe'
+$checksum32  = ''
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
