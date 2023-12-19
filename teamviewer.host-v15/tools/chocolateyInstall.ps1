ecfb51852b0f38967849701196fe1080d0ee25d8
Chocolatey <chocolatey@realdimensions.net>
2023-12-19 22:01:14 +0000

    AU: 1 updated - teamviewer.host [skip ci] https://gist.github.com/codingsteff/11d4abbe6bb1fcd50426bad5b7ea88de/8f31334dc41e23e3c8008b4e9027a98cbc61929b

--- teamviewer.host-v15/tools/chocolateyInstall.ps1
+++ teamviewer.host-v15/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'teamviewer.host'
 $url32       = 'https://download.teamviewer.com/download/version_15x/TeamViewer_Host_Setup.exe'
-$checksum32  = 'b4fa5fcf51cef886667f5dca32cb316a2957ecb30ff3affe463b4912a458ca77'
+$checksum32  = '9995f52ae5bc88b15a86f214f21d25b0b02e72c56b5d7daa61d4a2a5a7b55083'
 
 $packageArgs = @{
   packageName            = $packageName
